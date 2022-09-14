895c585aa4c1aaf6841f2bd19b2d6b2d465f8a0e
mark-savik <99361212+mark-savik@users.noreply.github.com>
2022-09-14 21:54:35 +0200

    add publishingHouse property to book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -12,6 +12,7 @@ namespace HomeLibrary.BusinessLogic.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public string PublishingHouse { get; set; }
         public string? Genre { get; set; }
         public string? Edition { get; set; }
         public string? SeriesTitle { get; set; }
