4b54c5c28e448a253c38822499940a59ae2b42db
mark-savik <99361212+mark-savik@users.noreply.github.com>
2023-04-03 20:56:19 +0200

    Home Library WebbApp  MVC project add

---
+++ HomeLibrary.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeLibrary.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
